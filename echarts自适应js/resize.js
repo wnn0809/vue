00a5a1ecb0d70b6cf12c362da133e7686063a3fa
w1418899532 <w1418899532@163.com>
2019-11-22 09:59:21 +0800

    echarts的自适应文件

---
+++ echarts自适应js/resize.js
@@ -0,0 +1,22 @@
+import { debounce } from '@/utils'
+
+export default {
+  mounted() {
+    this.__resizeHandler = debounce(() => {
+      if (this.chart) {
+        this.chart.resize()
+      }
+    }, 100)
+    window.addEventListener('resize', this.__resizeHandler)
+  },
+  beforeDestroy() {
+    window.removeEventListener('resize', this.__resizeHandler)
+  },
+  methods: {
+    sidebarResizeHandler(e) {
+      if (e.propertyName === 'width') {
+        this.__resizeHandler()
+      }
+    }
+  }
+}
